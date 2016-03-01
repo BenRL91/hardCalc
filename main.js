f943373b239cfa96eed2e98fe2e6f8a4ea2e9633
Ben Lee <leeohsunshine@gmail.com>
2016-03-01 18:24:48 -0500

    Made buttons clicked display
    made clear button functional

--- main.js
+++ main.js
@@ -1,14 +1,9 @@
-// button.addEventListener('click', add, sub, mul, div);
-// button.addEventListener('click');
+var clear, display, one, two, three, four, five, six, seven, eight, nine, zero,
+equals, decimal, addition, subtraction, multiplication, division;
 
-var equals, operation, display, one, two, three, four, five, six, seven, eight, nine, zero;
-
-display = document.querySelector('#display');
-button = document.querySelector('.button');
-clear = document.querySelector('#clear');
-equals = eval(display.value);
-one = document.querySelector('.one')
-two = document.querySelector('.two')
+display = document.querySelector('.display');
+one = document.querySelector('.one');
+two = document.querySelector('.two');
 three = document.querySelector('.three')
 four = document.querySelector('.four')
 five = document.querySelector('.five')
@@ -17,25 +12,70 @@ seven = document.querySelector('.seven')
 eight = document.querySelector('.eight')
 nine = document.querySelector('.nine')
 zero = document.querySelector('.zero')
-function addToDisplay(event) {
-	
+clear = document.querySelector('.clear')
+
+function moveToDisplay1(event) {
+	var total = display.textContent + "1";
+	display.textContent = total
 }
 
-button = function makeButton (event) {
-	one = one.textContent
-	two = two.textContent
-	three = three.textContent
-	four = four.textContent
-	five = five.textContent
-	six = six.textContent
-	seven = seven.textContent
-	eight = eight.textContent
-	nine = nine.textContent
-	zero = zero.textContent
-
+function moveToDisplay2(event) {
+	var total = display.textContent + "2";
+	display.textContent = total
 }
 
 
+function moveToDisplay3(event) {
+	var total = display.textContent + "3";
+	display.textContent = total
+}
 
-addEventListener('click', button)
+function moveToDisplay4(event) {
+	var total = display.textContent + "4";
+	display.textContent = total
+}
 
+function moveToDisplay5(event) {
+	var total = display.textContent + "5";
+	display.textContent = total
+}
+
+function moveToDisplay6(event) {
+	var total = display.textContent + "6";
+	display.textContent = total
+}
+
+function moveToDisplay7(event) {
+	var total = display.textContent + "7";
+	display.textContent = total
+}
+
+function moveToDisplay8(event) {
+	var total = display.textContent + "8";
+	display.textContent = total
+}
+
+function moveToDisplay9(event) {
+	var total = display.textContent + "9";
+	display.textContent = total
+}
+
+function moveToDisplay0(event) {
+	var total = display.textContent + "0";
+	display.textContent = total
+}
+
+function clearDisplay(event) {
+	display.textContent = ""
+}
+one.addEventListener('click', moveToDisplay1)
+two.addEventListener('click', moveToDisplay2) 
+three.addEventListener('click', moveToDisplay3)
+four.addEventListener('click', moveToDisplay4)
+five.addEventListener('click', moveToDisplay5)
+six.addEventListener('click', moveToDisplay6)
+seven.addEventListener('click', moveToDisplay7)
+eight.addEventListener('click', moveToDisplay8)
+nine.addEventListener('click', moveToDisplay9)
+zero.addEventListener('click', moveToDisplay0)
+clear.addEventListener('click', clearDisplay)
