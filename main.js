59426cb6d287b2794ff45012341c99c67080244f
Ben Lee <leeohsunshine@gmail.com>
2016-03-01 16:56:42 -0500

    Starting Hard Mode Calc

--- main.js
+++ main.js
@@ -1,22 +1,41 @@
-function add(event) {
-	total = Number(num_one.value) + Number(num_two.value)
-	answer.textContent = total
+// button.addEventListener('click', add, sub, mul, div);
+// button.addEventListener('click');
+
+var equals, operation, display, one, two, three, four, five, six, seven, eight, nine, zero;
+
+display = document.querySelector('#display');
+button = document.querySelector('.button');
+clear = document.querySelector('#clear');
+equals = eval(display.value);
+one = document.querySelector('.one')
+two = document.querySelector('.two')
+three = document.querySelector('.three')
+four = document.querySelector('.four')
+five = document.querySelector('.five')
+six = document.querySelector('.six')
+seven = document.querySelector('.seven')
+eight = document.querySelector('.eight')
+nine = document.querySelector('.nine')
+zero = document.querySelector('.zero')
+function addToDisplay(event) {
+	
 }
 
-function sub(event) {
-	total = Number(num_one.value) - Number(num_two.value)
-	answer.textContent = total
+button = function makeButton (event) {
+	one = one.textContent
+	two = two.textContent
+	three = three.textContent
+	four = four.textContent
+	five = five.textContent
+	six = six.textContent
+	seven = seven.textContent
+	eight = eight.textContent
+	nine = nine.textContent
+	zero = zero.textContent
+
 }
 
-function mul(event) {
-	total = Number(num_one.value) * Number(num_two.value)
-	answer.textContent = total
-}
 
-function div(event) {
-	total = Number(num_one.value) / Number(num_two.value)
-	display.textContent = total
-}
 
-button.addEventListener('click', add, sub, mul, div);
+addEventListener('click', button)
 
